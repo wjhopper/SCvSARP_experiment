1a83fc3cbdd42d85aa14d5064ccfd0d29d8f3c18
whopper <wjhopper510@gmail.com>
2017-01-25 21:03:14 -0500

    manually specify the vertical placement of the text, instead of leaving it up to PTB. PTBs calculations when one string had a 'low' letter and the other didnt meant the cue and the target were not vertically aligned

--- drawCueTarget.m
+++ drawCueTarget.m
@@ -1,8 +1,8 @@
 function drawCueTarget(cue, target, window, constants )
 
-DrawFormattedText(window, cue ,'right', 'center',[],[],[],[],[],[],constants.left_half-[0 0 constants.spacing 0]);
-DrawFormattedText(window,' - ', 'center','center');
-DrawFormattedText(window, target , constants.right_half(1)+constants.spacing, 'center');
+DrawFormattedText(window, cue ,'right', constants.yCenter,[],[],[],[],[],[],constants.left_half-[0 0 constants.spacing 0]);
+DrawFormattedText(window,' - ', 'center',constants.yCenter);
+DrawFormattedText(window, target , constants.right_half(1)+constants.spacing, constants.yCenter);
 
 end
 
