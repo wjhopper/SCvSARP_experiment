5160d42ed4788a7258b6301024271327112cc315
whopper <wjhopper510@gmail.com>
2017-02-10 10:40:54 -0500

    draw target words and responses in uppercase

--- drawCueTarget.m
+++ drawCueTarget.m
@@ -2,7 +2,7 @@ function drawCueTarget(cue, target, window, constants )
 
 DrawFormattedText(window, cue ,'right', constants.yCenter,[],[],[],[],[],[],constants.left_half-[0 0 constants.spacing 0]);
 DrawFormattedText(window,' - ', 'center',constants.yCenter);
-DrawFormattedText(window, target , constants.right_half(1)+constants.spacing, constants.yCenter);
+DrawFormattedText(window, upper(target) , constants.right_half(1)+constants.spacing, constants.yCenter);
 
 end
 
