fc72f797ec870e89e02de2bfe2c9d2f23cf62fd7
whopper <wjhopper510@gmail.com>
2017-03-30 10:26:05 -0400

    fixed bug where reset variable was unset if color parameter was not passed to drawCueTarget

--- drawCueTarget.m
+++ drawCueTarget.m
@@ -3,6 +3,8 @@ function drawCueTarget(cue, target, window, constants, color)
 if nargin == 5 && all(size(color) == [1,3]) && isnumeric(color)
     reset = true;
     old_color = Screen('TextColor', window, floor(color));
+else
+    reset = false;
 end
 
 DrawFormattedText(window, cue ,'right', constants.yCenter,[],[],[],[],[],[],constants.left_half-[0 0 constants.spacing 0]);
