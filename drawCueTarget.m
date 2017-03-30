a4288bd55d97804daf728206b663fa20efbe5539
whopper <wjhopper510@gmail.com>
2017-03-30 08:53:14 -0400

    allow the color of cue word text to be set in drawCueTarget

--- drawCueTarget.m
+++ drawCueTarget.m
@@ -1,6 +1,14 @@
-function drawCueTarget(cue, target, window, constants )
+function drawCueTarget(cue, target, window, constants, color)
+
+if nargin == 5 && all(size(color) == [1,3]) && isnumeric(color)
+    reset = true;
+    old_color = Screen('TextColor', window, floor(color));
+end
 
 DrawFormattedText(window, cue ,'right', constants.yCenter,[],[],[],[],[],[],constants.left_half-[0 0 constants.spacing 0]);
+if reset
+    Screen('TextColor', window, old_color);
+end
 DrawFormattedText(window,' - ', 'center',constants.yCenter);
 DrawFormattedText(window, upper(target) , constants.right_half(1)+constants.spacing, constants.yCenter);
 
